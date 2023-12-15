1fa09e32f82db215b0961a5b388c1936e01fad63
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-12-15 22:44:36 +0100

    added form to set post

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -6,14 +6,16 @@ use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(MicroPostRepository $posts, EntityManagerInterface $entityManager): Response
+    public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
             'posts' => $posts->findAll()
@@ -26,5 +28,34 @@ class MicroPostController extends AbstractController
         return $this->render("micro_post/show.html.twig", [
             "post" => $post
         ]);
+    }
+
+    #[Route("/micro-post/add", name: "micro_post_add", priority: 2)]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+            ->add("title")
+            ->add("text")
+            ->add("submit", SubmitType::class, ["label" => "Save"])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            $this->addFlash("success", "Your micro post has been added");
+            return $this->redirectToRoute("app_micro_post");
+
+        }
+
+        return $this->render("micro_post/add.html.twig", [
+            "form" => $form
+        ]);
+
     }
 }
