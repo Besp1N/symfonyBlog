444b0f4f53543f7be75af0025e721a13d4ec04a1
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-12-28 13:01:06 +0100

    added one to one ralation with user and userProfile

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,13 +17,28 @@ class HelloController extends AbstractController
         ["message" => "hi", "created" => "2023/04/12"],
         ["message" => "bye", "created" => "2021/01/14"]
     ];
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
-    public function index(int $limit): Response
+    #[Route('/', name: 'app_index')]
+    public function index(UserProfileRepository $profiles, EntityManagerInterface $entityManager): Response
     {
+//        $user = new User();
+//        $user->setEmail("dupa@wp.pl");
+//        $user->setPassword("123456789");
+//
+//       $profile = new UserProfile();
+//       $profile->setUser($user);
+//
+//       $entityManager->persist($profile);
+//       $entityManager->flush();
+
+//        $profile = $profiles->find(1);
+//        $entityManager->remove($profile);
+//        $entityManager->flush();
+
+
        return $this->render(
            "hello/index.html.twig", [
                "messages" => $this->messages,
-               "limit" => $limit
+               "limit" => 3
        ]);
     }
 
